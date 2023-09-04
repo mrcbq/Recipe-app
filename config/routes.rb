0083c3d0231fd75a906f7faf7cec7d54a8915f8e
mrcbq <marcobaquero@gmail.com>
2023-09-04 17:35:16 -0500

    Add routes to recipe_foods, foods, recipes, public_recipes, general_shopping_list

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :foods
-  resources :recipes
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :recipe_foods, only: [ :destroy, :new, :create ]
+  resources :foods, only: [ :index, :new, :create, :destroy ]
+  resources :recipes, only: [ :index, :show, :create, :destroy, :new ]
+  get '/public_recipes', to: 'recipes#public_recipes'
+  get '/general_shopping_list', to: 'foods#general_shopping_list'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
