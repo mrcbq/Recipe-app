505e89387cabe43c353c963121cdb613cc4b072e
mrcbq <marcobaquero@gmail.com>
2023-09-05 15:47:21 -0500

    Add logic to controller for index, show, new, create and destroy methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes
   end
 
   # GET /recipes/1 or /recipes/1.json
@@ -12,10 +12,11 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+    @recipe.user = current_user
   end
 
-  # GET /recipes/1/edit
-  def edit; end
+  # # GET /recipes/1/edit
+  # def edit; end
 
   # POST /recipes or /recipes.json
   def create
@@ -32,18 +33,18 @@ class RecipesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipes/1 or /recipes/1.json
-  def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /recipes/1 or /recipes/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @recipe.update(recipe_params)
+  #       format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @recipe }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @recipe.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /recipes/1 or /recipes/1.json
   def destroy
