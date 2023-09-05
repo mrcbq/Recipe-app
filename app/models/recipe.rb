f2a171448329a924c224af04ee8468b65e1f04bc
mrcbq <marcobaquero@gmail.com>
2023-09-05 11:56:50 -0500

    Fix typo in food controller, Remove public type in Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,5 +6,5 @@ class Recipe < ApplicationRecord
   validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :cooking_time, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true, length: { maximum: 200 }
-  validates :public, presence: true, inclusion: { in: [true, false] }
+  validates :public, inclusion: { in: [true, false] }
 end
