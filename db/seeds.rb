1bbd9facce9562f385b5137275bd0743ac98dfc2
mrcbq <marcobaquero@gmail.com>
2023-09-05 11:02:04 -0500

    Add some seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create(name: "Nacho")
+recipe1 = Recipe.create(name: "Rice and Egg", preparation_time: 1.5, cooking_time: 0.5, description: "Egg and rice", public: true, user: user1)
+food1 = Food.create(name: "Egg", measurement_unit: "units", quantity: 14, price: 20.5, user: user1)
