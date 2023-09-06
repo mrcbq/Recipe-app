abf657e74e5a62b60340f1c657b444b849f2935f
mrcbq <marcobaquero@gmail.com>
2023-09-06 10:04:08 -0500

    Add logic to remove button in show recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,10 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @recipe_food = RecipeFood.where(recipe_id: params[:id])
+    # p @recipe_food
+  end
 
   # GET /recipes/new
   def new
