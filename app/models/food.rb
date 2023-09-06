cec5ffc65c99512c6fe9f280a1d39d1259f6881f
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-06 14:38:11 -0300

    Fix ForeingKey error on deleting food
    Co-authored-by: mrcbq <marcobaquero@gmail.com>

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,15 @@
 class Food < ApplicationRecord
+  before_destroy :delete_associated_recipe_foods
+
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def delete_associated_recipe_foods
+    recipe_foods.destroy_all
+  end
 end
