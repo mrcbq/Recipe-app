42a79badf51dc96a6c28c72d97f14281d9e6cfd9
mrcbq <marcobaquero@gmail.com>
2023-09-06 12:34:43 -0500

    Fix destroy method for recipes add method to destroy all ingredients related

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,7 @@ class RecipesController < ApplicationController
   def show
     @recipe_food = RecipeFood.where(recipe_id: params[:id])
     # p @recipe_food
+    @recipe = Recipe.find(params[:id])
   end
 
   # GET /recipes/new
