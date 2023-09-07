ce9cac6cba391f981ffe287ea6436c8b4c200ffd
mrcbq <marcobaquero@gmail.com>
2023-09-07 16:22:16 -0500

    Add recipe index specs

--- spec/views/recipe/index.html.erb_spec.rb
+++ spec/views/recipe/index.html.erb_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe 'recipe/index', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'recipe/index', type: :system do
+  describe 'index page' do
+
+    let!(:user) { User.create(name: 'John', email: "jhon@jhon.com", password: "111111", password_confirmation: "111111") }
+    
+    let!(:recipe1) { Recipe.create(name: 'Chocolate with bread', preparation_time: '2', cooking_time: '3', description: 'A delicious beverage', public: true, user_id: user.id) }
+    
+    let!(:recipe2) { Recipe.create(name: 'Risotto with meat', preparation_time: '2', cooking_time: '3', description: 'A delicious plate', public: true, user_id: user.id) }
+
+    before do
+      sign_in user
+      recipes = [recipe1, recipe2]
+      visit recipes_path
+    end
+
+    it 'displays a list of recipes' do
+      expect(page).to have_content(recipe1.name)
+      expect(page).to have_content(recipe2.name)
+    end
+
+    it 'Display button remove' do
+      expect(page).to have_link('New recipe')
+    end
+
+    it 'Check correct url' do
+      expect(page).to have_link('New recipe', href: new_recipe_path)
+    end
+  end
 end
