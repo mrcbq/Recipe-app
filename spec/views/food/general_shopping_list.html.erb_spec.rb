0bf983155bc3ad070e2c28247fb5a06f824ff297
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-07 16:33:40 -0300

    write tests for Food#general_shopping_list view

--- spec/views/food/general_shopping_list.html.erb_spec.rb
+++ spec/views/food/general_shopping_list.html.erb_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe 'food/general_shopping_list', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'General Shopping List', type: :system do
+  describe 'Shopping List page' do
+    let!(:user) { User.create(name: 'Nacho', email: 'nachofino98@gmail.com', password: 'nachofino1',
+       password_confirmation: 'nachofino1') }
+    
+    let!(:food1) { Food.create(name: 'Milk', measurement_unit: 'Liters', price: 20,
+       quantity: 1, user_id: user.id) }
+    
+    let!(:recipe) {Recipe.create(name: 'Milk recipe', preparation_time: 1, cooking_time: 0.5,
+       description: 'Milk recipe description', public: true, user_id: user.id) }
+    
+    let!(:recipe_food) { RecipeFood.create(food_id: food1.id, recipe_id: recipe.id, quantity: 3)}
+
+    before do
+      sign_in user
+      visit general_shopping_list_path
+    end
+
+    it 'displays the view title' do
+      expect(page).to have_css('h1', text: 'General Shopping List')
+    end
+
+    it 'displays the amount of food items to buy' do
+      expect(page).to have_content('Amount of food items to buy: 1')
+    end
+
+    it 'displays the total value of food needed' do
+      expect(page).to have_content('Total value of food needed: 40.0')
+    end
+
+    it 'displays the name and quantity of the items to buy' do
+      expect(page).to have_content('Milk')
+      expect(page).to have_content('2.0 Liters')
+      expect(page).to have_content('$ 40.0')
+    end
+
+    it 'displays the table headers' do
+      expect(page).to have_content('Food')
+      expect(page).to have_content('Quantity')
+      expect(page).to have_content('Price')
+    end
+  end
 end
